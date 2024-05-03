51703f2a81d5fe90db8a587a5742bc8362e302af
johntrigg <60859958+johntrigg@users.noreply.github.com>
2024-05-03 17:41:22 -0400

    Fixes

--- extractThemesAndIcons.sh
+++ extractThemesAndIcons.sh
@@ -2,8 +2,8 @@
 
 cp -r linux-rice /tmp
 tar -xvf /tmp/linux-rice/themes/Sweet-Dark-v40.tar.xz
-sudo cp -r /tmp/linux-rice/themes/Sweet-Dark-v40 /usr/share/themes
+sudo cp -r Sweet-Dark-v40 /usr/share/themes
 
 
 tar -xvf /tmp/linux-rice/icons/candy-icons.tar.xz
-sudo cp -r /tmp/linux-rice/icons/candy-icons /usr/share/icons
+sudo cp -r candy-icons /usr/share/icons
